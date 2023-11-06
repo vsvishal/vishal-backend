4efd129b62aa61b81fb4e73e62769df844af82b6
Vishal Sharma <39647664+vsvishal@users.noreply.github.com>
2023-11-06 09:11:51 +0530

    db connection added

---
+++ src/index.js
@@ -0,0 +1,32 @@
+// require("dotenv").config({ path: "./env" });
+import dotenv from "dotenv";
+
+import connectDB from "./db/index.js";
+
+connectDB();
+
+dotenv.config({
+  path: "./env",
+});
+
+/* // APPROACH ONE
+const app = express()
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("ERROR: ", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`App is running on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("ERROR: ", error);
+    throw error;
+  }
+})();
+
+*/
